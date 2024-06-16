d8081b3d75687e6395c3d79f1f9e37e464cb5518
quang <nguyenthequang080203@gmail.com>
2024-06-16 17:55:02 +0700

    set up boilerplate streaming code

--- main.py
+++ main.py
@@ -1,7 +1,20 @@
 import logging
+from quixstreams import Application
+from uuid import uuid4
 
 def main():
-    pass
+    app = Application(
+        broker_address = "localhost:9092",
+        loglevel = "DEBUG",
+        consumer_group = str(uuid4()),
+        auto_offset_reset = "earliest",
+     )
+
+    input_topic = app.topic("weather_data_demo")
+
+    sdf = app.dataframe(input_topic)
+
+    app.run(sdf)
 
 if __name__ == "__main__":
     logging.basicConfig(level="DEBUG")
