5f046abc3736ebe5dd0558a9e7204f0d90f90259
quang <nguyenthequang080203@gmail.com>
2024-06-17 18:21:54 +0700

    add reducer and initializer, processing the messages before sending to gg sheets

--- main.py
+++ main.py
@@ -1,6 +1,29 @@
 import logging
 from quixstreams import Application
 from uuid import uuid4
+from datetime import timedelta
+
+def initializer_fn(msg): # get a single msg from the very start of the hour and has to turn it into a summary
+    temperature = msg['current']['temperature_2m']
+
+    return {
+        "open": temperature,
+        "high": temperature,
+        "low": temperature,
+        "close": temperature
+     }
+
+def reducer_fn(summary, msg): # take a the summary so far and the new message then return a new summary
+    # take the temp out of the new msg first
+    temperature = msg['current']['temperature_2m']
+    
+    return {
+        "open": summary['open'], # the open value is always the first ever value
+        "high": max(summary['high'], temperature) , # the maximum of what we've seen
+        "low": min(summary['low'], temperature),
+        "close": temperature, # the last value we know
+     }
+
 
 def main():
     app = Application(
@@ -13,7 +36,25 @@ def main():
     input_topic = app.topic("weather_data_demo")
 
     sdf = app.dataframe(input_topic)
+    
+    #sdf = sdf.group_into_hourly_batches()
+    sdf = sdf.tumbling_window(duration_ms=timedelta(hours=1)) # an hour long block of data
 
+    #sdf = sdf.summarize_that_hour()
+    sdf = sdf.reduce(
+        initializer = initializer_fn,
+        reducer = reducer_fn
+    ) # reduce hours worth of data to a single summary row
+    
+    # once you're done with the window processing calls, you need to finalize the window to get back into the regular data frame api
+    sdf = sdf.final()
+
+    # .update() is used to apply a function to each message
+    # for every frame, log out the message
+    sdf = sdf.update(lambda msg: logging.debug("Got: %s", msg))
+    #sdf = sdf.send_to_gg_sheets()
+    
+    # what the application will do now is go back and reprocess all the hours, we would get a series of hourly summaries that tell us the timestamp at the start and end of the hour and the value of our summary object. It would just sit there, waiting for more data to come in until the end of the hour then will finish off the processing and start waiting for the next hour worth of data.
     app.run(sdf)
 
 if __name__ == "__main__":
